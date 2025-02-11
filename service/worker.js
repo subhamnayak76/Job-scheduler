f269acc95a7d14e1271b685a7afd9bc8b0ff8db0
subhamnayak76 <subhamnayak9638@gmail.com>
2025-02-11 23:37:16 +0530

    added Locking

--- service/worker.js
+++ service/worker.js
@@ -2,18 +2,27 @@ const { Worker } = require("bullmq");
 const Job = require("../model/jobHandler");
 const nodemailer = require("nodemailer");
 const { REDIS_HOST, REDIS_PORT } = require("../config/config");
-
+const { Redis } = require('ioredis');  
+const RedLock = require('redlock').default;
+const connectDB = require('../utils/db')
+const redisClient = new Redis({ host: REDIS_HOST, port: REDIS_PORT });
+const redlock = new RedLock([redisClient], {
+  retryCount: 3,
+});
+connectDB()
 const jobWorker = new Worker("jobQueue", async (job) => {
   console.log(`Processing job: ${job.id}, Type: ${job.name}`);
-
+  const lockKey = `lock:worker:${job.id}`;
   try {
+    const lock = await redlock.acquire([lockKey], 30000)
+    console.log('locked achived')
     if (job.name === "email") {
-      const { to } = job.data.data;
-      await sendEmail(to);
+      const { to ,from,subject,message} = job.data.data;
+      await sendEmail(to,from,subject,message);
     }
 
     await Job.findByIdAndUpdate(job.data._id, { status: "completed" });
-
+    await lock.release();
     console.log(`Job ${job.id} completed`);
   } catch (error) {
     console.error(`Job ${job.id} failed:`, error);
@@ -24,17 +33,17 @@ const jobWorker = new Worker("jobQueue", async (job) => {
 console.log("Worker started...");
 
 // Email Sending Function
-async function sendEmail(to) {
+async function sendEmail(to,from,subject,message) {
   const transporter = nodemailer.createTransport({
     service: "Gmail",
     auth: { user: '', pass: '' },
   });
 
   await transporter.sendMail({
-    from: " ",
+    from ,
     to,
-    subject: "Test Email",
-    text: "This is a scheduled email.",
+    subject,
+    text:message
   });
 
   console.log(`Email sent to ${to}`);
